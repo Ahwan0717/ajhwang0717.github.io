2a1461efae83d1d45de690be8dc38c931e0780cc
Ariana Hwang <arianahwang@Arianas-MacBook-Pro.local>
2021-01-04 16:12:06 -0500

    Updates

---
+++ static/js/main.4d07e494.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonplatin-shui-site"]=this["webpackJsonplatin-shui-site"]||[]).push([[0],{9:function(t,e,n){"use strict";n.r(e);var i=n(0),s=n(1),c=n.n(s),r=n(3);var h=function(){return Object(i.jsx)("div",{children:Object(i.jsx)("h1",{children:"HOME"})})};n.n(r).a.render(Object(i.jsx)(c.a.StrictMode,{children:Object(i.jsx)(h,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.4d07e494.chunk.js.map
